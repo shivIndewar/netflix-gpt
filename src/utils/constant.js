1e325c8d043703be89bd757d25b53bdc5d88bc7a
Shivprasad Indewar <indewars@gmail.com>
2024-07-25 10:29:16 +0530

    modified changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -31,6 +31,4 @@ export const UPCOMMING_MOVIES =
 export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/8728e059-7686-4d2d-a67a-84872bd71025/e90516bd-6925-4341-a6cf-0b9f3d0c140a/IN-en-20240708-POP_SIGNUP_TWO_WEEKS-perspective_WEB_34324b52-d094-482b-8c2a-708dc64c9065_large.jpg";
 
 export const SUPPORTED_LANGUAGES = [{identifier:"en", name:"English"},{identifier:"hindi", name:"Hindi"},{identifier:"spanish", name:"Spaninsh"}];
-
-export const OPEN_AI_KEY = "sk-proj-OGPcvEGcnLm7tcezApQtT3BlbkFJvFPwMhdGkkpYC4fjPM6Q";
 export const SEARCH_MOVIES = "https://api.themoviedb.org/3/search/movie?query=Andaz&include_adult=false&language=en-US&page=1";
